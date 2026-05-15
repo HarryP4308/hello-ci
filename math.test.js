ea09d098792639d4cad24f4f04a5874af52e6db1
Harold <haroldewegbemi018@gmail.com>
2026-05-15 16:22:14 +0100

    add multiply function and test

--- math.test.js
+++ math.test.js
@@ -4,3 +4,7 @@ const { add } = require('./math');
 test('adds 2 + 3 to equal 5', () => {
   expect(add(2, 3)).toBe(5);
 });
+
+test('multiplies 3 x 4 to equal 12', () => {
+  expect(multiply(3, 4)).toBe(12);
+});
